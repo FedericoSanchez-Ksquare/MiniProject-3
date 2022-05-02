5e24f9fa302449a21105b24a652fe4586b59d7e9
Braulio Ochoa <braulio.ochoa@itksquare.edu.mx>
2022-05-02 14:17:13 -0500

    Transaction endpoint
    Added a description and a new balance and this is registered in a history balance.

--- dist/index.js
+++ dist/index.js
@@ -6,12 +6,24 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 //Inicial balance
 let currentBalance = 0;
+let transHistory = [];
 const PORT = 3000;
 const app = (0, express_1.default)();
 const endpoint = "/v1";
 app.use(express_1.default.json());
 app.get(endpoint + '/balance', (req, res) => {
     res.json({ currentBalance });
+});
+app.post(endpoint + '/transaction', (req, res) => {
+    let description = "";
+    let newBalance = 0;
+    newBalance = req.body.balance;
+    description = req.body.description;
+    const objTransHistory = { newBalance, description };
+    transHistory.push(objTransHistory);
+    console.log(newBalance, description);
+    console.log(transHistory);
+    res.send("Transaction processed correctly");
 });
 app.listen(PORT, () => {
     console.log('The application is listening on port', PORT);
--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import express from 'express';
 
 //Inicial balance
 let currentBalance:number = 0;
+let transHistory: Object [] = [];
 
 const PORT = 3000;
 const app = express();
@@ -9,11 +10,22 @@ const endpoint: string = "/v1";
 
 
 app.use(express.json());
+
 app.get(endpoint + '/balance', (req, res) => {
-  
     res.json({currentBalance})
 });
 
+app.post(endpoint + '/transaction', (req, res) => {
+    let description:string = "";
+    let newBalance:number = 0;
+    newBalance = req.body.balance;
+    description = req.body.description;
+    const objTransHistory: Object = {newBalance, description};
+    transHistory.push(objTransHistory);
+    console.log(newBalance, description);
+    console.log(transHistory);
+      res.send("Transaction processed correctly");
+  });
 
 app.listen(PORT, () => {
     console.log('The application is listening on port', PORT);
