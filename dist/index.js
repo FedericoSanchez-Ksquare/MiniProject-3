44aabef535adf07e435b1fe75a95b6edb3410646
Braulio Ochoa <braulio.ochoa@itksquare.edu.mx>
2022-05-02 18:09:34 -0500

    User story 7
    Added the button to get the balance and show the current balance.

--- dist/index.js
+++ dist/index.js
@@ -4,13 +4,15 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const cors_1 = __importDefault(require("cors"));
 //Inicial balance
 let currentBalance = 0;
 let transHistory = [];
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 const app = (0, express_1.default)();
 const endpoint = "/v1";
 app.use(express_1.default.json());
+app.use((0, cors_1.default)());
 app.get(endpoint + '/balance', (req, res) => {
     res.json({ currentBalance });
 });
