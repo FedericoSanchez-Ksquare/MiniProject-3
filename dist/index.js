26a587bf269b47cb06d01849ed96c9672c2b5549
Braulio Ochoa <braulio.ochoa@itksquare.edu.mx>
2022-05-02 13:57:39 -0500

    Transaction enpoint
    Added the transaction endpoint that received a description and an amount and this is added to the current balance.

--- dist/index.js
+++ dist/index.js
@@ -13,6 +13,14 @@ app.use(express_1.default.json());
 app.get(endpoint + '/balance', (req, res) => {
     res.json({ currentBalance });
 });
+app.post(endpoint + '/transaction', (req, res) => {
+    let description = "";
+    let newBalance = 0;
+    newBalance = req.body.balance;
+    description = req.body.description;
+    currentBalance += newBalance;
+    res.send("Transaction processed correctly");
+});
 app.listen(PORT, () => {
     console.log('The application is listening on port', PORT);
 });
--- src/index.ts
+++ src/index.ts
@@ -10,9 +10,17 @@ const endpoint: string = "/v1";
 
 app.use(express.json());
 app.get(endpoint + '/balance', (req, res) => {
-  
     res.json({currentBalance})
 });
+
+app.post(endpoint + '/transaction', (req, res) => {
+    let description:string = "";
+    let newBalance:number = 0;
+    newBalance = req.body.balance;
+    description = req.body.description;
+    currentBalance += newBalance
+      res.send("Transaction processed correctly");
+  });
 
 
 app.listen(PORT, () => {
