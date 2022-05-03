6d1e38185729f1ab8bdd9efe787414d0cdc825fa
FedericoSanchez-Ksquare <102171026+FedericoSanchez-Ksquare@users.noreply.github.com>
2022-05-02 21:13:13 -0500

    change urls

--- dist/index.js
+++ dist/index.js
@@ -4,7 +4,6 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
-const cors_1 = __importDefault(require("cors"));
 //Inicial balance
 let currentBalance = 0;
 let transHistory = [];
@@ -12,7 +11,6 @@ const PORT = process.env.PORT || 3000;
 const app = (0, express_1.default)();
 const endpoint = "/v1";
 app.use(express_1.default.json());
-app.use((0, cors_1.default)());
 app.get(endpoint + '/balance', (req, res) => {
     res.json({ currentBalance });
 });
