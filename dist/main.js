4bf2f494cb7bbed5c32c3263124cdef548439478
Braulio Ochoa <braulio.ochoa@itksquare.edu.mx>
2022-05-02 20:54:26 -0500

    User Story 9
    Added the explanation of the positve and negative balances

--- dist/main.js
+++ dist/main.js
@@ -8,20 +8,18 @@ const getBalance = async () => {
     headers: { 'Content-Type': 'application/json' },})
     const data = await res.json();
     console.log(data)
-    balance.innerHTML = data.currentBalance
+    balance.innerHTML = "$" + data.currentBalance
 }
 
 const postTransaction = async () => {
     const desc = descriptionVal.value
     const val = +amountVal.value
     const url = `http://localhost:3000/v1/transaction`;
-    if(desc === "" && val ==="")
-    {
-    }else{
+    if(desc === "" && val ==="")return;
+    
         await fetch(url,{method: 'POST',headers: { 'Content-Type': 'application/json' },body: JSON.stringify({ description: desc, balance: val })})
         document.getElementById("amount").value = ""
         document.getElementById("description").value =""
-    }
 }
 
 const clear = async  () =>{
